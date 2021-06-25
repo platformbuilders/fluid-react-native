4a0c1d2f2ec691d0fa1160d43deeac67fbae62e5
Lucas Diniz <md.lucasd@gmail.com>
2021-06-25 10:28:32 -0300

    fix test components

---
+++ src/components/Icon/__test__/Icon.spec.tsx
@@ -0,0 +1,18 @@
+import 'jest';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components/native';
+import Icon from '..';
+import { theme } from '../../../test/helpers';
+
+describe('<Icon />', () => {
+  it('should render icon component correctly', () => {
+    const render = renderer.create(
+      <ThemeProvider theme={theme}>
+        <Icon name="access-point" id="testing" accessibility="testing_icon" />
+      </ThemeProvider>,
+    );
+
+    expect(render.toJSON()).toMatchSnapshot();
+  });
+});
