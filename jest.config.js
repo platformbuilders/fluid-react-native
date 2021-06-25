57b94c09834268be6e2dc0e2d3999f78d5762270
Lucas Diniz <md.lucasd@gmail.com>
2021-06-25 10:28:13 -0300

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,40 +1,18 @@
-const { defaults: tsjPreset } = require('ts-jest/presets');
-
 module.exports = {
-  ...tsjPreset,
-  verbose: true,
-  automock: false,
-  collectCoverage: true,
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/config/*',
-    '!<rootDir>/src/theme/index.ts',
-    '!<rootDir>/src/types/index.ts',
-    '!<rootDir>/src/components/index.ts',
-    '!<rootDir>/src/utils/helpers/index.ts',
-    '!**/*.d.ts'
+  preset: 'react-native',
+  moduleDirectories: ['node_modules', 'src'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    '<rootDir>/src/test/mocks.ts',
   ],
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx)',
-    '**/?(*.)+(spec|test).+(ts|tsx)'
-  ],
-  coverageDirectory: 'coverage',
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/coverage/',
-    '/dist/'
-  ],
-  testEnvironment: 'jsdom',
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFilesAfterEnv: ['<rootDir>/src/config/jest.setup.ts', 'jest-canvas-mock'],
-  transform: {
-    ...tsjPreset.transform,
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
+  cacheDirectory: '.jest/cache',
+  coverageThreshold: {
+    global: {
+      branches: 55,
+      functions: 55,
+      lines: 75,
+      statements: 50,
     },
   },
-}
+};
