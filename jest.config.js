de212e7ec72c5abf2eb0e176489bb3a0d18b1224
Michael Amaral <michael.amaral@platformbuilders.io>
2021-06-29 15:14:02 -0300

    collecting coverage

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,21 @@ module.exports = {
     '<rootDir>/src/test/mocks.ts',
   ],
   cacheDirectory: '.jest/cache',
+  collectCoverage: true,
   coverageThreshold: {
     global: {
       branches: 55,
-      functions: 55,
-      lines: 75,
+      functions: 50,
+      lines: 65,
       statements: 50,
     },
   },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/assets/**',
+    '!**/__tests__/**',
+    '!**/documentation/**',
+    '!**/storybook/**',
+  ],
 };
