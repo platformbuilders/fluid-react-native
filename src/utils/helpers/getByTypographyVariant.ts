0d8634805fbaeb3adb2ae92d75d09c51e4c37592
Michael Amaral <michael.amaral@platformbuilders.io>
2021-07-26 23:14:11 -0300

    chore(test): updating snapshots, removing font scaling

--- src/utils/helpers/getByTypographyVariant.ts
+++ src/utils/helpers/getByTypographyVariant.ts
@@ -1,12 +1,14 @@
+import { toNumber } from 'lodash';
+import { PixelRatio } from 'react-native';
 import { getTheme } from '@platformbuilders/helpers';
-import { scaledFontSize } from './fontScale';
+// import { TypographyVariantStyle } from '../../types';
 
 export const getFontSize = (props: any): number => {
-  const fontSize = getTheme(`${props.variant}.fontSize`)(props) || 16;
-  return scaledFontSize(fontSize);
+  const fontSizeFromTheme = getTheme(`${props.variant}.fontSize`)(props);
+  return PixelRatio.roundToNearestPixel(toNumber(fontSizeFromTheme)) || 16;
 };
 
 export const getLineHeight = (props: any): number => {
   const lineHeight = getTheme(`${props.variant}.lineHeight`)(props) || 22;
-  return scaledFontSize(lineHeight);
+  return PixelRatio.roundToNearestPixel(toNumber(lineHeight));
 };
