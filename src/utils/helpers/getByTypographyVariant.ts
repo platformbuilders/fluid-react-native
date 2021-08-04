8604237e782a9c28b71d3f0251bde50d682a0970
Lucas Diniz <md.lucasd@gmail.com>
2021-08-04 18:55:08 -0300

    chore(wp-95): fix contrast colors typography

--- src/utils/helpers/getByTypographyVariant.ts
+++ src/utils/helpers/getByTypographyVariant.ts
@@ -11,9 +11,7 @@ export const getFontSize = (props: any): number => {
   );
 };
 
-export const getLineHeight = (props: any): number => {
-  const lineHeight =
-    (((getTheme(`fontSizes.${props.variant}`)(props) as unknown) as number) ||
-      baseFontSize) * 1.4;
+export const getLineHeight = (): number => {
+  const lineHeight = baseFontSize * 1.4;
   return PixelRatio.roundToNearestPixel(toNumber(lineHeight));
 };
