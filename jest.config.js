e7428066323f4e56fa843355c1967f44bd328005
Du Spada <eduardo.spada@platformbuilders.io>
2021-09-20 18:53:15 -0300

    fix(ref): version patch

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 75,
+      functions: 70,
+      lines: 80,
+      statements: 80,
     },
   },
   collectCoverageFrom: [
