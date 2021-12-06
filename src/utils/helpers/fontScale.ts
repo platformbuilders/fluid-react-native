d9110f77cbefbc74001f890f9db81b2ac93f6f6d
Michael Amaral <michael.amaral@platformbuilders.io>
2021-12-05 21:33:38 -0300

    feat(improvements): adding Markdown component, removing font-scaling, local isIOS, bumping version

--- src/utils/helpers/fontScale.ts
+++
@@ -1,9 +0,0 @@
-import { RFValue } from 'react-native-responsive-fontsize';
-
-export const scaledFontSize = (
-  size: number | string,
-  baselineFactor?: 280,
-): number => {
-  const value = typeof size === 'string' ? parseInt(size, 10) : size;
-  return RFValue(value, baselineFactor);
-};
