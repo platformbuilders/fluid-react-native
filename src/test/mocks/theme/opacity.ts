621a7f7e3cf300df71d3d0b591567897053d1ca7
Lucas Diniz <md.lucasd@gmail.com>
2022-03-23 09:28:51 -0300

    fix lint

--- src/test/mocks/theme/opacity.ts
+++ src/test/mocks/theme/opacity.ts
@@ -1,8 +1,8 @@
 export default {
-  disabled: 30,
-  empty: 50,
-  overlay: 80,
-  opaque: 100,
-  transparent: 0,
-  subtle: 10,
+  disabled: '30',
+  empty: '50',
+  overlay: '80',
+  opaque: '100',
+  transparent: '0',
+  subtle: '10',
 };
