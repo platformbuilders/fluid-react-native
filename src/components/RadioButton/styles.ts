9d01444dd813a6b70f4c5866cae170881c293fc0
guilhermebauer-builders <guilherme.bauer@platformbuilders.io>
2022-07-08 12:59:05 -0300

    refactor(radio): design

--- src/components/RadioButton/styles.ts
+++ src/components/RadioButton/styles.ts
@@ -4,6 +4,7 @@ import { getTheme, ifStyle } from '../../utils/helpers';
 import Touchable from '../Touchable';
 
 const textColor = getTheme('text.main');
+const checkedRadioColor = getTheme('brand.primary.main');
 const hasColor = ifStyle('radioButtonColor');
 const hasCheckedColor = ifStyle('checkedRadioButtonColor');
 
@@ -16,9 +17,9 @@ export const Radio = styled(Touchable)<RadioProps>`
   width: ${({ size }: RadioProps) => moderateScale(size)}px;
   height: ${({ size }: RadioProps) => moderateScale(size)}px;
   border-radius: ${({ size }: RadioProps) => moderateScale(size / 2)}px;
-  border: ${moderateScale(1)}px
+  border: ${moderateScale(2)}px
     ${({ radioButtonColor }: RadioProps): any =>
-      hasColor(radioButtonColor, textColor)};
+      hasColor(radioButtonColor, textColor)}${hasColor('', '4D')};
   align-items: center;
   justify-content: center;
 `;
@@ -34,7 +35,7 @@ export const CheckedRadio = styled.View<CheckRadioProps>`
   height: ${({ internalSize }: CheckRadioProps) =>
     moderateScale(internalSize)}px;
   background-color: ${({ checkedRadioButtonColor }: CheckRadioProps): any =>
-    hasCheckedColor(checkedRadioButtonColor, textColor)};
+    hasCheckedColor(checkedRadioButtonColor, checkedRadioColor)};
   border-radius: ${({ internalSize }: CheckRadioProps) =>
     moderateScale(internalSize / 2)}px;
 `;
