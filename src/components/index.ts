8f9a125e742b4c1c10cdc569f74d4b6bb4e59b73
guilhermebauer-builders <guilherme.bauer@platformbuilders.io>
2022-09-20 16:53:55 -0300

    feature(toggle): component and tests

--- src/components/index.ts
+++ src/components/index.ts
@@ -26,3 +26,4 @@ export { default as LabelDivider } from './LabelDivider';
 export { default as Header } from './Header';
 export { TextInputMask, MaskService, TextMask } from './MaskedText';
 export { default as Navbar } from './Navbar';
+export { default as Toggle } from './Toggle';
