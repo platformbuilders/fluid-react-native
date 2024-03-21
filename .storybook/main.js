1563939ae8367db19c87abda3ee70659c001ef0d
marcelxsilva <maarcelomonteiro@gmail.com>
2024-03-21 13:00:14 -0300

    feat: storybook

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+/** @type{import("@storybook/react-native").StorybookConfig} */
+module.exports = {
+  stories: ["../src/components/**/*.stories.?(ts|tsx|js|jsx)"],
+  addons: ["@storybook/addon-ondevice-controls", "@storybook/addon-ondevice-notes"],
+};
