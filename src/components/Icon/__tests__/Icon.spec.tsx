7683ac2766c1350d1a16f6c5bcaf844bff144f40
marcelxsilva <maarcelomonteiro@gmail.com>
2024-03-25 08:42:42 -0300

    feat: doc icon

---
+++ src/components/Icon/__tests__/Icon.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components/native';
+import Icon from '..';
+import theme from '../../../theme';
+
+describe('<Icon />', () => {
+  it('should render icon component correctly', () => {
+    const render = renderer.create(
+      <ThemeProvider theme={theme}>
+        <Icon name="book" id="testing" accessibility="testing_icon" />
+      </ThemeProvider>,
+    );
+
+    expect(render.toJSON()).toMatchSnapshot();
+  });
+});
---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,92 @@
+import React, { FC } from 'react';
+import { Animated, View } from 'react-native';
+import FontAwesome5Icons from 'react-native-vector-icons/FontAwesome5';
+import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as SvgIcons from '../../assets/svg';
+import { IconFonts } from '../../enums';
+import { IconType } from '../../types';
+import If from '../If';
+import Touchable from '../Touchable';
+import { FaBrands, FaLight, FaRegular, FaSolid } from './FontAwesomeProIcons';
+
+export const Icon: FC<IconType> = ({
+  id,
+  accessibility,
+  accessibilityLabel,
+  testID,
+  name = '',
+  touchable = true,
+  isEmoji = false,
+  size = 20,
+  color = undefined,
+  style = [{}],
+  onPress = (): void => {},
+  borderColor = 'transparent',
+  backgroundColor = 'transparent',
+  type = IconFonts.FontAwesome,
+  iconSets,
+  width,
+  height,
+  ...rest
+}) => {
+  const iconName = name?.charAt(0).toUpperCase() + name?.slice(1);
+  const bundleName = `Icon${iconName}` as keyof typeof SvgIcons;
+  const svgIcons: any = SvgIcons[bundleName];
+  const Svg: any = iconSets ? iconSets[bundleName] : svgIcons;
+
+  const iconSet = {
+    [IconFonts.FontAwesome]: FontAwesome5Icons,
+    [IconFonts.Material]: MaterialIcons,
+    [IconFonts.FABrands]: FaBrands,
+    [IconFonts.FALight]: FaLight,
+    [IconFonts.FARegular]: FaRegular,
+    [IconFonts.FASolid]: FaSolid,
+  };
+
+  const IconComponent = iconSet[type] as any;
+
+  return (
+    <Animated.View style={style}>
+      <Touchable
+        id={id || accessibility}
+        accessibility={accessibility || iconName}
+        accessibilityLabel={accessibilityLabel || accessibility}
+        testID={testID || id || accessibility}
+        disabled={!touchable}
+        onPress={onPress}
+        {...rest}
+      >
+        <View>
+          <If condition={isEmoji}>
+            <Animated.Text
+              style={{
+                fontSize: size,
+                color,
+                borderColor,
+                backgroundColor,
+                width,
+                height,
+              }}
+            >
+              {name}
+            </Animated.Text>
+          </If>
+          {Svg && (
+            <Svg
+              width={width || size}
+              height={height || size}
+              color={color}
+              borderColor={borderColor}
+              backgroundColor={backgroundColor}
+            />
+          )}
+          <If condition={!isEmoji && !Svg}>
+            <IconComponent name={name} color={color} size={size} />
+          </If>
+        </View>
+      </Touchable>
+    </Animated.View>
+  );
+};
+
+export default Icon;
