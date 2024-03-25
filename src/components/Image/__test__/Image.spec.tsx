9fbc40137359cbf0a8d25d6c906f230b7eb86a8d
marcelxsilva <maarcelomonteiro@gmail.com>
2024-03-25 08:42:15 -0300

    feat: doc image

---
+++ src/components/Image/__test__/Image.spec.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components/native';
+import Image from '..';
+import theme from '../../../theme';
+
+const uri =
+  'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+
+describe('<Image />', () => {
+  it('should render Image component correctly', () => {
+    const render = renderer.create(
+      <ThemeProvider theme={theme}>
+        <Image source={{ uri }} id="testing" accessibility="testing_image" />
+      </ThemeProvider>,
+    );
+
+    expect(render.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with no placeholder', () => {
+    const render = renderer.create(
+      <ThemeProvider theme={theme}>
+        <Image
+          id="testing"
+          accessibility="testing_image"
+          displayPlaceholder={false}
+          source={{ uri }}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(render.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with custom placeholder animation color', () => {
+    const render = renderer.create(
+      <ThemeProvider theme={theme}>
+        <Image
+          id="testing"
+          accessibility="testing_image"
+          animationColor="red"
+          source={{ uri }}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(render.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with custom placeholder animation and container colors', () => {
+    const render = renderer.create(
+      <ThemeProvider theme={theme}>
+        <Image
+          id="testing"
+          accessibility="testing_image"
+          animationColor="red"
+          animationContainerColor="blue"
+          source={{ uri }}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(render.toJSON()).toMatchSnapshot();
+  });
+});
