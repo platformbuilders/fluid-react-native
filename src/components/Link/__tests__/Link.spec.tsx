7ed7c8944bd14ee48ac87053b4657c0d4adea451
marcelxsilva <maarcelomonteiro@gmail.com>
2024-03-25 10:23:51 -0300

    feat: doc link

---
+++ src/components/Link/__tests__/Link.spec.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import { fireEvent, render } from 'react-native-testing-library';
+import renderer from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components/native';
+import Link from '..';
+import theme from '../../../theme';
+
+describe('<Link />', () => {
+  it('should render link', () => {
+    const wrapper = renderer.create(
+      <ThemeProvider theme={theme}>
+        <Link id="testing" accessibility="" />
+      </ThemeProvider>,
+    );
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('should trigger onPress function', () => {
+    const onPressEvent = jest.fn();
+
+    const { getByTestId } = render(
+      <ThemeProvider theme={theme}>
+        <Link id="testing" accessibility="" onPress={onPressEvent} />
+      </ThemeProvider>,
+    );
+
+    const component = getByTestId('testing');
+
+    fireEvent.press(component);
+
+    expect(onPressEvent).toHaveBeenCalled();
+  });
+});
