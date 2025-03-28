633bee03e160ac7b9729493547a6686e379f416c
Mike Amaral <michael.amaral@platformbuilders.io>
2025-03-28 18:20:34 -0300

    chore: configurar ESLint para ignorar arquivos de teste

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   extends: '@platformbuilders/eslint-config-builders/react',
-  ignorePatterns: ['jest'],
+  ignorePatterns: [
+    'jest',
+    'src/**/*.spec.{ts,tsx}',
+    'src/**/__tests__/**/*',
+    'src/**/__test__/**/*',
+    'dist',
+    'node_modules'
+  ],
 };
